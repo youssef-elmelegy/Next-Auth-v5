7a28e2adbdacd2ec8723fc661aeb7fa5119b868f
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-10-07 14:25:10 +0300

    force -dynamic

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { Suspense } from "react";
 import LoginForm from "@/components/auth/login-form";
 
+export const dynamic = "force-dynamic";
+
 const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 };
 
 export default LoginPage;
